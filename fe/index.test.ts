f176e3ddfd9c8fb5cb4fffeaeedddb2cd82906d0
yusuke46 <96458067+yusuke46@users.noreply.github.com>
2022-02-14 23:16:30 +0900

    アップデート

--- fe/index.test.ts
+++ fe/index.test.ts
@@ -1,6 +1,3 @@
 import * as index from './index';
 
-test('add', () => {
-    expect(index.plus(1, 2)).toBe(3);
-    expect(index.plus(1, -2)).toBe(-1);
-});
+test('add', () => {});
--- fe/index.ts
+++ fe/index.ts
@@ -1,11 +1,6 @@
 'use strict';
 import './style.scss';
 
-export function plus(a, b) {
-    return a + b;
-}
-
-const a: string = 'fuga1';
-document.addEventListener('DOMContentLoaded', () => {
-    console.log(plus(1, 2));
+addEventListener('load', function () {
+    alert('犬');
 });
