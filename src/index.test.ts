33dca3264b9f9b3ee7551fc36c53b780793762f7
Koki Fukuda <ko.fu.dev@gmail.com>
2022-02-14 18:52:00 +0900

    Prepare unit testing

---
+++ src/index.test.ts
@@ -0,0 +1,6 @@
+import * as index from './index'
+
+test('add', () => {
+    expect(index.plus(1, 2)).toBe(3);
+    expect(index.plus(1, -2)).toBe(-1);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 'use strict';
 import './style.scss'
 
+export function plus(a, b) {
+    return a + b;
+}
+
 const a: string = 'fuga1';
 document.addEventListener('DOMContentLoaded', () => {
-    console.log(a);
+    console.log(plus(1, 2));
 });
